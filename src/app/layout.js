c7c386a14e75161377c8dcee561fa389bce7f497
satwikbhasin <satwikbhasin@gmail.com>
2025-01-31 02:42:02 -0500

    Add attribution link to dashboard with external link icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,10 @@ import dynamic from "next/dynamic";
 import { cookies } from "next/headers";
 import Navbar from "@/components/common/navbar";
 import Loader from "@/components/common/loader";
+import Link from "next/link";
 import { GeistSans } from 'geist/font/sans'
 import "./globals.css";
+import { ExternalLink } from "lucide-react";
 
 const ThemeProvider = dynamic(() => import("@/styling/theming/themeProvider"), {
   ssr: false,
@@ -30,12 +32,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${GeistSans.variable} antialiased`}
+        className={`${GeistSans.variable} antialiased relative`}
         suppressHydrationWarning
       >
         <ThemeProvider initialTheme={themeMode}>
           <Navbar />
           {children}
+          <div className="cursor-default flex absolute top-3 left-1/2 transform -translate-x-1/2 font-bold text-white bg-zinc-950/50 backdrop-blur-lg px-10 py-1 rounded-full animate-presence z-1000000">
+            Dashboard By
+            <Link className="text-green-200 flex flex-row ml-1 gap-1" href="https://satwikbhasin.com">
+              Satwik Bhasin
+              <ExternalLink strokeWidth={3} size={18} />
+            </Link>
+          </div>
           <Analytics />
           <BeamAnalytics />
         </ThemeProvider>
