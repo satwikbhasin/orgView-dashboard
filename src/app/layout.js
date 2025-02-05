c150bfcfa647dfa8c69ec2d15578fc02f85d6294
satwikbhasin <satwikbhasin@gmail.com>
2025-02-05 14:50:01 -0500

    Update metadata in layout.js for improved SEO and social sharing

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import dynamic from "next/dynamic";
 import { cookies } from "next/headers";
 import Navbar from "@/components/common/navbar";
 import Loader from "@/components/common/loader";
-import { GeistSans } from 'geist/font/sans'
+import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
 const ThemeProvider = dynamic(() => import("@/styling/theming/themeProvider"), {
@@ -12,23 +12,27 @@ const ThemeProvider = dynamic(() => import("@/styling/theming/themeProvider"), {
   loading: () => <Loader />,
 });
 
-export const metadata = {
-  title: "Org View Modern Dashboard for your business",
-  description:
-    "Org View is a modern dashboard for your business. It is a powerful and feature-rich dashboard that will help you to monitor your business.",
-  image: "/orgView_logo.png",
-  url: "https://orgview.vercel.app",
-  type: "website",
-  siteName: "Org View",
-  keywords: ["dashboard template", "business", "modern", "orgview", "ui/ux"],
-  creator: "Satwik Bhasin",
-};
-
 export default function RootLayout({ children }) {
   const themeModeCookie = cookies().get("themeMode");
   const themeMode = themeModeCookie ? themeModeCookie.value : "light";
+
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta property="og:title" content="Org View - Modern Dashboard for Your Business" />
+        <meta
+          property="og:description"
+          content="Org View is a modern dashboard to help you monitor and grow your business with powerful insights."
+        />
+        <meta property="og:image" content="https://orgview.vercel.app/orgView_logo.svg" />
+        <meta property="og:url" content="https://orgview.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Org View" />
+        <meta name="description" content="Org View is a modern business dashboard with powerful insights." />
+        <meta name="keywords" content="dashboard template, business, modern, orgview, UI/UX" />
+        <meta name="author" content="Satwik Bhasin" />
+        <title>Org View - Modern Dashboard for Your Business</title>
+      </head>
       <body
         className={`${GeistSans.variable} antialiased relative`}
         suppressHydrationWarning
